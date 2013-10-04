4098ee535fb560a152aecad0d7e05a6ba2c6fc9a
FreeBz <freebz@hananet.net>
2013-10-04 11:59:37 +0900

    Basic Object-Oriented Programming in Scala

---
+++ ch05/abstract-types-script.scala
@@ -0,0 +1,15 @@
+// abstract-types-script.scala
+
+import java.io._
+
+abstract class BulkReader {
+  // ...
+}
+
+class StringBulkReader(val source: String) extends BulkReader {
+  // ...
+}
+
+class FileBulkReader(val source: File) extends BulkReader {
+  // ...
+}
